d66107148ae61f71a98e740c5ab8b30137919467
MD. Mehrab Hossain <38918084+jansir17@users.noreply.github.com>
2022-07-05 15:25:53 +0600

    history fucntionality

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Calculator from "./Counterr";
+
 function App() {
   return <Calculator/>
 }
--- src/Counterr.js
+++ src/Counterr.js
@@ -1,26 +1,33 @@
 import {useState} from "react";
+
+let arr= [];
 export default function Calculator(){
   const [value, setValue]= useState(()=>{return 0});
 
-
   const handleChange= (e)=>{
     if(e.target.innerHTML==='-100'){
-    setValue(value-100)
+    setValue(value-100);
+    arr.push(value+e.target.innerHTML);
   }
   else if(e.target.innerHTML==='-10'){
     setValue(value-10)
+    arr.push(value+e.target.innerHTML);
   }
   else if(e.target.innerHTML==='-1'){
     setValue(value-1)
+    arr.push(value+e.target.innerHTML);
   }
   else if(e.target.innerHTML==='+1'){
     setValue(value+1)
+    arr.push(value+e.target.innerHTML);
   }
   else if(e.target.innerHTML==='+10'){
     setValue(value+10)
+    arr.push(value+e.target.innerHTML);
   }
   else if(e.target.innerHTML==='+100'){
     setValue(value+100)
+    arr.push(value+e.target.innerHTML);
   }
 }
 
@@ -30,10 +37,24 @@ export default function Calculator(){
         <button type="button" onClick={handleChange}>-100</button>
         <button type="button" onClick={handleChange}>-10</button>
         <button type="button" onClick={handleChange}>-1</button>
-        <h4>Current Value:</h4><h1>{value}</h1>
+        <div>
+        <br></br>
+        <button type='button' >Undo</button>
+        <span><h4>{value}</h4></span>
+        <button type='button' >Redo</button>
+        </div>
+        <br></br>
         <button type="button" onClick={handleChange}>+1</button>
         <button type="button" onClick={handleChange}>+10</button>
         <button type="button" onClick={handleChange}>+100</button>
+        <br></br>
+        <br></br>
+        <div>History</div>
+        {arr.map((item, key)=>{
+          return(
+            <h6 key={key}>{item}</h6>
+          )
+        })}
         </center>
     </>
   )
